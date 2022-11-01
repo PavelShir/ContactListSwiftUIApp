7197f8046204f9d79c3fdebbb48462c608a8e451
Pavel Shirokiy <pshirokiy@gmail.com>
2022-11-01 23:02:49 +0300

    Initial Commit

---
+++ ContactListSwiftUIApp/ContactListSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIAppApp.swift
+//  ContactListSwiftUIApp
+//
+//  Created by 19543442 on 01.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
